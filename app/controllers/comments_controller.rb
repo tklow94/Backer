89f50cb337bc3f57de2018d0ce95d446c8f74603
Trevor <tlow94@uw.edu>
2020-10-27 18:29:24 -0700

    tailwind css

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,10 @@
 class CommentsController < ApplicationController
   before_action :authenticate_user!
 
+
+  def new
+  end
+  
   def create
     @comment = @commentable.comments.new(comment_params)
     @comment.user = current_user
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ require 'sidekiq/web'
 
 Rails.application.routes.draw do
   get 'comments/create'
-  resources :projects
+  resources :projects do
+      resources :comments, module: :projects
+  end
 
     authenticate :user, lambda { |u| u.admin? } do
       mount Sidekiq::Web => '/sidekiq'
