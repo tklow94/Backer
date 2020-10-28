a645a3db5cc5b1df9f46be87762f249cfdd8336a
Trevor <tlow94@uw.edu>
2020-10-28 16:06:42 -0700

    add fields to projects migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_28_001934) do
+ActiveRecord::Schema.define(version: 2020_10_28_225840) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -73,6 +73,9 @@ ActiveRecord::Schema.define(version: 2020_10_28_001934) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "current_donation_amount", default: 0
+    t.datetime "expires_at", default: "2020-11-27 23:06:24"
+    t.string "status", default: "active"
     t.index ["user_id"], name: "index_projects_on_user_id"
   end
 
