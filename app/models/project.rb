5744319e008d11d17f1d07832e8cc685a30c952e
Trevor <tlow94@uw.edu>
2020-10-27 17:29:35 -0700

    fix relationship syntax error

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,5 +2,5 @@ class Project < ApplicationRecord
   belongs_to :user
   has_rich_text :description
   has_one_attached :thumbnail
-  has-many :comments, as: :commentable
+  has_many :comments, as: :commentable
 end
