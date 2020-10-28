7bd98884b7c08b4dcfaac707bb8d991e4920a335
Trevor <tlow94@uw.edu>
2020-10-28 16:10:44 -0700

    add expire project method

--- app/jobs/expire_project_job.rb
+++ app/jobs/expire_project_job.rb
@@ -1,7 +1,17 @@
 class ExpireProjectJob < ApplicationJob
   queue_as :default
 
-  def perform(*args)
-    # Do something later
+  def perform(project)
+    @project = project
+    return if project_already_inactive?
+
+    @project.status = "inactive"
+    @project.save!
+  end
+
+
+  private
+  def project_already_inactive?
+    @project.status == "inactive"
   end
 end
