26c75dc6fcf26915badfb566d4f094d7590aa897
Trevor <tlow94@uw.edu>
2020-10-29 16:04:38 -0700

    stripe implemented

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,10 @@
 class User < ApplicationRecord
-has_person_name
-
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_provider: [:stripe_connect]
+         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:stripe_connect]
   has_many :projects, dependent: :destroy
+
+  def can_receive_payments?
+    uid? &&  provider? && access_code? && publishable_key?
+  end
+
 end
