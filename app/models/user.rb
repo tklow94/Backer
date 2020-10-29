bdce11c4e7f164f5a4a4d66f4a0568708f8ce849
Trevor <tlow94@uw.edu>
2020-10-29 15:13:41 -0700

    add stripe keys

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
 has_person_name
 
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :omniauthable, aomiauth_provider: [:stripe_connect]
   has_many :projects, dependent: :destroy
 end
