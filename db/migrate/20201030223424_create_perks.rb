f0f0793b4a8da328442a55c0c61655a0f390d3c8
Trevor <tlow94@uw.edu>
2020-10-30 15:42:39 -0700

    syntax errors

--- db/migrate/20201030223424_create_perks.rb
+++ db/migrate/20201030223424_create_perks.rb
@@ -2,7 +2,7 @@ class CreatePerks < ActiveRecord::Migration[6.0]
   def change
     create_table :perks do |t|
       t.string :title
-      t.decimal :amount, precision: 8, scale: 2 default: 0
+      t.decimal :amount, precision: 8, scale: 2, default: 0
       t.text :description
       t.integer :quantity
       t.references :project, null: false, foreign_key: true
